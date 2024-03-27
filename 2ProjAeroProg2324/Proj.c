fae2b90c0cb751c243293cb2b603fd709ff28bf9
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2024-03-27 23:00:45 +0000

    protypes

--- 2ProjAeroProg2324/Proj.c
+++ 2ProjAeroProg2324/Proj.c
@@ -9,75 +9,6 @@
 Airport_list *airports_list = NULL;
 Flight_list *flights_list = NULL;
 
-void read_airports(FILE *airports){
-
-    char line[200];
-    while (fgets(line, sizeof(line), airports) != NULL){
-        
-        Airport *airport = malloc(sizeof(Airport));
-        airport->ICAO = malloc(5 * sizeof(char));
-        airport->IATA = malloc(4 * sizeof(char));
-        airport->city = malloc(50 * sizeof(char));
-       
-        sscanf(line,"%s %s %d %d %d %c %d %d %d %c %s %d", airport->ICAO, airport->IATA, 
-        &airport->latitude.degrees, &airport->latitude.minutes, &airport->latitude.seconds, &airport->latitude.direction,
-        &airport->longitude.degrees, &airport->longitude.minutes, &airport->longitude.seconds, &airport->longitude.direction, 
-        airport->city, &airport->timezone); 
-        
-        airport->next = NULL;
-        airport->prev = NULL;
-        
-        if (airports_list->head == NULL) {
-            airports_list->head = airport;
-            airports_list->tail = airport;
-        }else {
-            airports_list->head->prev = airport;
-            airport->next = airports_list->head;
-            airports_list->head = airport;
-        }
-    }
-}
-
-
-void read_fligths(FILE *routes){
-    char line[200];
-    char* current_airline = malloc(10 * sizeof(char));
-    while (fgets(line, sizeof(line), routes) != NULL){
-        
-        if (sscanf(line,"AIRLINE: %s", current_airline) == 1){}
-        else{
-            Flight *flight = malloc(sizeof(Flight));
-            flight->airline = malloc(10 * sizeof(char));
-            flight->flight_code = malloc(10 * sizeof(char));
-            flight->depart_IATA= malloc(5 * sizeof(char));
-            flight->arrival_IATA = malloc(5 * sizeof(char));
-            sscanf(line,"%s %s %d:%d %s %d:%d", flight->flight_code, flight->depart_IATA,
-            &flight->depart_time_hour, &flight->depart_time_minute, flight->arrival_IATA,
-            &flight->arrival_time_hour, &flight->arrival_time_minute);
-            
-            strcpy(flight->airline, current_airline);
-
-            flight->next = NULL;
-            flight->prev = NULL;
-
-            if (flights_list->head == NULL) {
-                flights_list->head = flight;
-                flights_list->tail = flight;
-            }else {
-                flights_list->head->prev = flight;
-                flight->next = flights_list->head;
-                flights_list->head = flight;
-            }
-        }
-    }
-    free(current_airline);
-}
-
-        
-
-
-
-
 int main(int argc, char *argv[]) {
 
     FILE *airports, *routes;
@@ -126,6 +57,69 @@ int main(int argc, char *argv[]) {
             printf("Invalid number of arguments\n");
             break;
     }
+}
+
+void read_airports(FILE *airports){
+
+    char line[200];
+    while (fgets(line, sizeof(line), airports) != NULL){
+        
+        Airport *airport = malloc(sizeof(Airport));
+        airport->ICAO = malloc(5 * sizeof(char));
+        airport->IATA = malloc(4 * sizeof(char));
+        airport->city = malloc(50 * sizeof(char));
+       
+        sscanf(line,"%s %s %d %d %d %c %d %d %d %c %s %d", airport->ICAO, airport->IATA, 
+        &airport->latitude.degrees, &airport->latitude.minutes, &airport->latitude.seconds, &airport->latitude.direction,
+        &airport->longitude.degrees, &airport->longitude.minutes, &airport->longitude.seconds, &airport->longitude.direction, 
+        airport->city, &airport->timezone); 
+        
+        airport->next = NULL;
+        airport->prev = NULL;
+        
+        if (airports_list->head == NULL) {
+            airports_list->head = airport;
+            airports_list->tail = airport;
+        }else {
+            airports_list->head->prev = airport;
+            airport->next = airports_list->head;
+            airports_list->head = airport;
+        }
+    }
+}
+
+void read_fligths(FILE *routes){
+    char line[200];
+    char* current_airline = malloc(10 * sizeof(char));
+    while (fgets(line, sizeof(line), routes) != NULL){
+        
+        if (sscanf(line,"AIRLINE: %s", current_airline) == 1){}
+        else{
+            Flight *flight = malloc(sizeof(Flight));
+            flight->airline = malloc(10 * sizeof(char));
+            flight->flight_code = malloc(10 * sizeof(char));
+            flight->depart_IATA= malloc(5 * sizeof(char));
+            flight->arrival_IATA = malloc(5 * sizeof(char));
+            sscanf(line,"%s %s %d:%d %s %d:%d", flight->flight_code, flight->depart_IATA,
+            &flight->depart_time_hour, &flight->depart_time_minute, flight->arrival_IATA,
+            &flight->arrival_time_hour, &flight->arrival_time_minute);
+            
+            strcpy(flight->airline, current_airline);
+
+            flight->next = NULL;
+            flight->prev = NULL;
+
+            if (flights_list->head == NULL) {
+                flights_list->head = flight;
+                flights_list->tail = flight;
+            }else {
+                flights_list->head->prev = flight;
+                flight->next = flights_list->head;
+                flights_list->head = flight;
+            }
+        }
+    }
+    free(current_airline);
 }
 
 void case_flights() {
--- 2ProjAeroProg2324/prototypes.h
+++ 2ProjAeroProg2324/prototypes.h
@@ -1,4 +1,6 @@
 void case_flights() ;
 void case_airports() ;
+void read_airports(FILE *airports) ;
+void read_fligths(FILE *routes) ;
 void show_flights(char *argv1, char *argv2, char *argv4) ;
 double calc_airport_distance(char *argv1, char *argv2) ;
